9770e2654f53d37a5c70d67a6d159d6d66cfb8d4
Desmond Carter <crtr@dsmnd.org>
2017-03-16 08:48:06 -0400

    Update styles for Resources, GetHelp, and GetInvolved. Add missing vendor prefixes for mobile breakpoints

--- src/components/GetHelp/GetHelp.js
+++ src/components/GetHelp/GetHelp.js
@@ -1,9 +1,41 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes, Component } from 'react'
 import styles from './GetHelp.css'
 //import MoreText from './../MoreText'
 
-const GetHelp = ({ getHelp }) => {
-    return (
+
+export default class GetHelp extends Component {
+    //state = {
+    //    return {
+    //        selections: []
+    //    }
+    //};
+    //onChildToggle = ({ id, selected }) => {
+     //   let selections = this.state.selections;
+     //   selections[id] = selected;
+     //   this.setState({
+     //       selections: selections
+     //   });
+    //};
+    //buildChildren = ({ dataItem }) => {
+        
+    
+    
+    
+    
+    
+    
+   // state = {
+   //     childVisible: false,
+   // };
+   // _onClick = ({ childVisible }) => {
+   //     this.setState({
+   //         childVisible: !this.state.childVisible,
+   //     });
+   //  }
+    render() {
+        {/*let hat = []*/}
+        const { getHelp } = this.props
+        return (
                 <div>
                     <div className={styles.title}>
                         <h1>Help is here</h1>
@@ -13,12 +45,21 @@ const GetHelp = ({ getHelp }) => {
                             <div className={styles.options}>
                                 <div className={styles.topics}> 
                                    <h2 className={styles.subtitle}> {helper.subtitle}</h2>
-                                   <div className={styles.description}>{helper.description}</div>
-                                   {/*<MoreText/> */}
-                                   {/* <div> {
-                                            getHelp.indexOf(helper) + " This is a string"
+                                   <div className={styles.description}> {helper.description}</div>
+                                   
+                                   
+                                   {/*<div>
+                                        {
+                                           "childVisible" + getHelp.indexOf(helper)
                                         }
-                                   </div> */}
+                                   </div>*/}
+                                   
+                                   {/*<div onClick={this._onClick}>
+                                        hat
+                                   </div>*/}
+                                   {/*
+                                       this.state.childVisible && <div className={styles.description}>{helper.description}</div>
+                                   */}
                                 </div>
                                 <img className={styles.imgs} src={helper.image} />
 
@@ -26,11 +67,10 @@ const GetHelp = ({ getHelp }) => {
                         ))} 
                     </div>
                 </div>
-    )
+        )
+    }
 }
 
 GetHelp.propTypes = {
    getHelp: PropTypes.array.isRequired,
 }
-
-export default GetHelp
