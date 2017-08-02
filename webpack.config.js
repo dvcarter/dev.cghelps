65dd061146e0eaeb888a7bd2d40d28f199d958df
Desmond Carter <crtr@dsmnd.org>
2017-08-02 11:45:30 -0400

    resolve netlify-cms issues

--- webpack.config.js
+++ webpack.config.js
@@ -235,7 +235,9 @@ export default (config = {}) => {
         },
       }),
       */
-
+      new CopyWebPackPlugin([
+              {from: 'admin', to: 'admin'},
+      ]),
       new PhenomicLoaderFeedWebpackPlugin({
         // here you define generic metadata for your feed
         feedsOptions: {
