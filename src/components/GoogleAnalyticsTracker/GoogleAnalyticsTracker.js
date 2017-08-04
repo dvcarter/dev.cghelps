c025791b763f481ddd4ba23a8b8b1b4f34fd722f
Desmond Carter <crtr@dsmnd.org>
2017-08-04 14:55:38 -0400

    reset window as undefined

--- src/components/GoogleAnalyticsTracker/GoogleAnalyticsTracker.js
+++ src/components/GoogleAnalyticsTracker/GoogleAnalyticsTracker.js
@@ -5,7 +5,7 @@ import ga from "react-google-analytics"
 const GoogleAnalyticsInitiailizer = ga.Initializer
 
 const isProduction = process.env.NODE_ENV === "production"
-const isClient = typeof window !== "undefined"
+const isClient = typeof window == "undefined"
 
 class GoogleAnalyticsTracker extends Component {
 
